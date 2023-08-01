e46d4eecdeadb985e5ee03834365ec5127eb4838
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-08-01 21:55:10 +0000

    Add Beeline interface

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
+#include <string>
+#include <iterator>
+#include <iostream>
+
 #include "cli.hpp"
+#include "beeline.hpp"
+
+
+std::string read_all_from(std::istream& input)
+{
+    input >> std::noskipws;
+    std::istream_iterator<char> it(input);
+    std::istream_iterator<char> end;
+    return {it, end};
+}
 
 
 int main(const int argc, const char** argv)
 {
     Arguments arguments = ArgumentParser().parse(argc, argv);
     init_logging(arguments.logging_level);
+    Beeline{}.run(read_all_from(std::cin));
 }
