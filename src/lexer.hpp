302a36c8fda5b79bea8487ebac7218800786c403
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-08-03 17:26:42 +0000

    Extract lexer to separate translation unit

---
+++ src/lexer.hpp
@@ -0,0 +1,108 @@
+#pragma once
+
+#include <vector>
+#include <variant>
+#include <memory>
+#include <cstddef>
+
+#include "beeline.hpp"
+
+
+struct Token
+{
+    enum class Type
+    {
+        // Single-character tokens.
+        LEFT_PARENTHESIS,
+        RIGHT_PARENTHESIS,
+        LEFT_BRACE,
+        RIGHT_BRACE,
+        MINUS,
+        PLUS,
+        SLASH,
+        STAR,
+
+        // One or two character tokens.
+        BANG,
+        BANG_EQUAL,
+        EQUAL,
+        EQUAL_EQUAL,
+        GREATER,
+        GREATER_EQUAL,
+        LESS,
+        LESS_EQUAL,
+
+        // Literals.
+        IDENTIFIER,
+        STRING,
+        NUMBER,
+
+        // Keywords.
+        AND,
+        OR,
+        IF,
+        ELSE,
+        TRUE,
+        FALSE,
+        NIL,
+        PRINT,
+        VAR,
+        WHILE,
+
+        // End of file.
+        END_OF_FILE,
+    };
+
+    struct Position
+    {
+        const std::size_t offset;
+        const std::size_t line;
+        const std::size_t column;
+        const std::size_t length;
+    };
+
+    using Literal = std::variant<std::nullptr_t, std::string, double, bool>;
+
+    const Type type;
+    const std::string lexeme;
+    const Literal literal;
+    const Position position;
+};
+
+
+class Lexer
+{
+public:
+    Lexer() = delete;
+    ~Lexer();
+    Lexer(const std::string& input);
+    const std::vector<Token>& scan();
+private:
+    class Impl;
+    std::unique_ptr<Impl> impl_;
+};
+
+
+class BeelineSyntaxError : public BeelineError
+{
+public:
+    BeelineSyntaxError(const std::string& message, const Token::Position& position);
+    const Token::Position& position() const noexcept;
+private:
+    const Token::Position position_;
+};
+
+
+class BeelineLexingError : public BeelineError
+{
+public:
+    BeelineLexingError(const std::string& message);
+};
+
+
+std::ostream& operator<<(std::ostream& os, const Token::Type& type);
+std::ostream& operator<<(std::ostream& os, const Token::Position& position);
+std::ostream& operator<<(std::ostream& os, const Token::Literal& literal);
+std::ostream& operator<<(std::ostream& os, const Token& token);
+std::ostream& operator<<(std::ostream& os, const BeelineSyntaxError& bse);
+std::ostream& operator<<(std::ostream& os, const BeelineLexingError& ble);
