d91bd72d9bd24ece4be560fe2c54b1868f07015c
Callum Curtis <105407637+callumcurtis@users.noreply.github.com>
2023-08-05 20:09:29 +0000

    Add parser implementation class

--- src/parser.cpp
+++ src/parser.cpp
@@ -1,5 +1,14 @@
+#include <vector>
+
 #include "parser.hpp"
+#include "lexer.hpp"
 
 
 class Parser::Impl
 {
+public:
+    Impl(std::vector<Token> tokens) : tokens_(tokens) {}
+private:
+    const std::vector<Token> tokens_;
+    std::size_t current_token_index_{0};
+};
